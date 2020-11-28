6a3a30411e21a0e7c83af6879ed5ddef3390ec5d
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-28 18:53:55 +0000

    feat: banner component

---
+++ build/precache-manifest.c9d7108ab889d21c000aec9ad5d3f68a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68be3782949b844952b5b39ab07c63a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20bc05eee897e3dd68da",
+    "url": "/static/css/main.36036297.chunk.css"
+  },
+  {
+    "revision": "d90e85501adf1bcc0842",
+    "url": "/static/js/2.9835ac7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9835ac7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bc05eee897e3dd68da",
+    "url": "/static/js/main.230ac143.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "5abf4eef09cda13e867c939211b11955",
+    "url": "/static/media/image-banner.5abf4eef.jpg"
+  }
+]);
